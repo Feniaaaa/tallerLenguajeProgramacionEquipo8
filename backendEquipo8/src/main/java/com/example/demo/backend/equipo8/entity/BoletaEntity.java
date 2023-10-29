800263882fdb906801a1f395b8531f552c0b3838
Feniaa <Fernandonicolascrucessanhueza@gmail.com>
2023-10-29 20:22:52 -0300

    Creacion de entidades boleta y pago, incluyendo la FK

---
+++ backendEquipo8/src/main/java/com/example/demo/backend/equipo8/entity/BoletaEntity.java
@@ -0,0 +1,39 @@
+package com.example.demo.backend.equipo8.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name="boletas")
+public class BoletaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank
+    @Column(name = "monto")
+    private String monto;
+    
+    @NotBlank
+    @Column(name = "fecha")
+    private String fecha;
+
+
+
+    public BoletaEntity(@JsonProperty("id") int id,@JsonProperty("monto") String monto, @JsonProperty("fecha") String fecha) {
+        super();
+        this.id = id;
+        this.monto = monto;
+        this.fecha = fecha;
+    }
+
+}
+
