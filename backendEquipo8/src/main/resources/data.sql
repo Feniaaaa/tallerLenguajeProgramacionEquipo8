4f54f6373a2677b4fc66f41465d86ffee8f49622
Feniaa <Fernandonicolascrucessanhueza@gmail.com>
2023-11-13 23:40:17 -0300

    Script SQL con inserto de Datos, localidad y tipoboleta

---
+++ backendEquipo8/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+
+
+
+Insert into tipoboleta (id,nombre) VALUES (1,'Factura');
+Insert into tipoboleta (id,nombre) VALUES (2,'Boleta');
+
+Insert into localidad (id,ciudad) VALUES (1,'Concepcion');
+Insert into localidad (id,ciudad) VALUES (2,'Talcahuano');
+Insert into localidad (id,ciudad) VALUES (3,'Hualpen');
+Insert into localidad (id,ciudad) VALUES (4,'Coronel');
