52d01ae629294148c597d2464be5b744715062f4
Feniaa <Fernandonicolascrucessanhueza@gmail.com>
2023-11-24 20:42:15 -0300

    implementacion de filtro crud boleta

--- backendEquipo8/src/main/java/com/example/demo/backend/equipo8/repository/IBoletaRepository.java
+++ backendEquipo8/src/main/java/com/example/demo/backend/equipo8/repository/IBoletaRepository.java
@@ -1,5 +1,8 @@
 package com.example.demo.backend.equipo8.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +11,5 @@ import com.example.demo.backend.equipo8.entity.BoletaEntity;
 @Repository
 public interface IBoletaRepository extends CrudRepository<BoletaEntity, Integer> {
 	
-	//UsuarioEntity findBylogin(String login);
+
 }
---
+++ backendEquipo8/src/main/resources/Datos.sql
@@ -0,0 +1,7 @@
+insert into tipoboleta (id,nombre) VALUES (1,"Factura");
+insert into tipoboleta (id,nombre) VALUES (2,"Boleta");
+
+insert into localidad (id,ciudad) VALUES (1,"Concepcion");
+insert into localidad (id,ciudad) VALUES (2,"Talcahuano");
+insert into localidad (id,ciudad) VALUES (3,"Hualpen");
+insert into localidad (id,ciudad) VALUES (4,"Coronel");
